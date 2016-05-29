514eda2177271cad43675c4d75c1575e631bcecd
Christina Gilbert <christina.gilbert.325@gmail.com>
2016-05-29 14:50:38 -0700

    added get_albums module for loading photos from facebook, downloaded opencv files, updated gitignore

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PhotoScraper
+
+Facebook Access Token -- To run the code, go to https://developers.facebook.com/tools-and-support/ and click on Graph API Explorer to get an access token. You'll need to ask for access to photos. Put this string into a file called API_KEYS.txt. I've included this file in my gitignore because I didn't want to post my person API key to github.
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-if __name__ == '__main__':
-	some_func()
+#import requests
+#import json
+from fb_albums import get_albums
 
-def some_func():
-	pass
+def access_graph_api():
+	token_file = open('API_KEYS.txt', 'r')
+	token = token_file.read()
+	api_url = "https://graph.facebook.com/v2.1/"
+	get_albums.download(token)
+	
+
+if __name__ == '__main__':
+	access_graph_api()
+
+
+
