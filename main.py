739e090bf2bcfb797edd936c2fc2f4a9b99be680
Christina Gilbert <christina.gilbert.325@gmail.com>
2016-05-29 19:31:26 -0700

    added facial tagging

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 #import requests
 #import json
 from fb_albums import get_albums
+from facial_recognition import face_detect
+from os import listdir
 
-def access_graph_api():
+PHOTOS_PATH = 'albums2'
+
+def download_photos():
 	token_file = open('API_KEYS.txt', 'r')
 	token = token_file.read()
 	api_url = "https://graph.facebook.com/v2.1/"
-	get_albums.download(token)
-	
+	get_albums.download(token, PHOTOS_PATH)
+
+def load_photos():
+	folders = listdir(PHOTOS_PATH)
+	for folder in folders:
+		photos = listdir(PHOTOS_PATH + "/" + folder)
+		for photo in photos:
+			process_photo(PHOTOS_PATH + "/" + folder + "/" + photo)
+
+def process_photo(filename):
+	print filename
+	face_detect.get_faces(filename)
 
 if __name__ == '__main__':
-	access_graph_api()
+	download_photos()
+	load_photos()
 
 
 
